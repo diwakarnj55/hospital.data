01109d6844ccaf6dd0167d4609184bba7303421c
diwakar <diwakar.nj55@gmail.com>
2024-09-18 16:45:10 +0530

    settengs updated

--- medicine/medicine/settings.py
+++ medicine/medicine/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'medicine.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test123',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
